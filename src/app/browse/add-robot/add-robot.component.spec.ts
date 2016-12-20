9a97d772398bf049d5164330a974d5779620634a
Barna Toth <Barna.Toth@logmein.com>
2016-12-20 17:18:30 +0100

    Progress

--- src/app/browse/add-robot/add-robot.component.spec.ts
+++ src/app/browse/add-robot/add-robot.component.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AddRobotComponent } from './add-robot.component';
 
@@ -11,7 +11,8 @@ describe('AddRobotComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddRobotComponent ]
+      declarations: [ AddRobotComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
