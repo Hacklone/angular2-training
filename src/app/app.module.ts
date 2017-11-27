521cf893941df6770cb081d601c561d94f4b05b2
Barna Toth <Barna.Toth@logmein.com>
2017-11-27 14:44:21 +0100

    Progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RobotListComponent } from './robot-list/robot-list.component';
+import { RobotCardComponent } from './shared/components/robot-card/robot-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RobotListComponent
+    RobotListComponent,
+    RobotCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/components/robot-card/robot-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Robot } from '../../models/robot.interface';
+
+@Component({
+  selector: 'app-robot-card',
+  templateUrl: './robot-card.component.html',
+  styleUrls: ['./robot-card.component.scss']
+})
+export class RobotCardComponent implements OnInit {
+
+  constructor() { }
+
+  @Input()
+  public robot: Robot;
+
+  ngOnInit() {
+  }
+
+}
